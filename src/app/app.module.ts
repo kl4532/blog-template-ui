687ee546edd63d900edf303cce9f8045457d9b30
kl4532 <kornellukasiak@gmail.com>
2020-10-24 23:17:35 +0200

    create routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from 'angularfire2/storage';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import {environment} from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PostsModule } from './posts/posts.module';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/blog', pathMatch: 'full'},
+  { path: '', loadChildren: './post/posts.module#PostsModule'},
+]
 
 @NgModule({
   declarations: [
@@ -23,10 +29,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AngularFirestoreModule,
     AngularFireAuthModule,
     AngularFireStorageModule,
-    AppRoutingModule,
     CoreModule,
     SharedModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    PostsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
