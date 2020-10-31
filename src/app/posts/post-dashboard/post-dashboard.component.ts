69b1cd1e3c315352bb0f367729a8caac2d1ee820
kl4532 <kornellukasiak@gmail.com>
2020-11-01 00:10:46 +0100

    add edit mode in dashboard

--- src/app/posts/post-dashboard/post-dashboard.component.ts
+++ src/app/posts/post-dashboard/post-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {AuthService} from '../../core/auth.service';
 import {PostService} from '../post.service';
 import {Observable} from 'rxjs';
@@ -6,6 +6,9 @@ import { AngularFireStorage } from 'angularfire2/storage';
 import {finalize} from 'rxjs/operators';
 import { NgxDropzoneModule } from 'ngx-dropzone';
 import {HttpClient} from '@angular/common/http';
+import {Post} from '../post';
+import {ActivatedRoute, Router} from '@angular/router';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-post-dashboard',
@@ -23,14 +26,29 @@ export class PostDashboardComponent implements OnInit {
   uploadFinished: boolean = true;
   downloadURL: Observable<string>;
 
+  modeTitle: string = 'Create post';
+  @Input() editMode: boolean = false;
+  @Input() post: Post;
+  @Output() editFinished: EventEmitter<boolean> = new EventEmitter<boolean>();
+  newImagesUrl: string[] = [];
+
+
   files: File[] = [];
 
   constructor(private authService: AuthService,
               private postService: PostService,
               private storage: AngularFireStorage,
-              private http: HttpClient) { }
+              private http: HttpClient,
+              private route: ActivatedRoute,
+              private router: Router,
+              private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
+    if (this.editMode) {
+        console.log('post', this.post);
+        this.modeTitle = 'Edit post';
+        this.fetchPostData();
+    };
   }
 
   createPost() {
@@ -42,16 +60,23 @@ export class PostDashboardComponent implements OnInit {
       published: new Date(),
       title: this.title
     };
-    console.log('data', data);
-
     this.postService.create(data);
     this.title = '';
     this.content = '';
+    this.files = [];
+    this.imagesUrl = [];
 
+    this.snackBar.open('Post created', 'Ok', {
+      duration: 3000,
+      panelClass: ['success-snackbar']
+    });
+    setTimeout(() => this.router.navigate(['/blog']),1000);
+  }
 
-    this.buttonText = 'Post Created';
-    setTimeout((() => this.buttonText = 'Create post'), 2000);
-
+  fetchPostData() {
+    this.title = this.post.title;
+    this.content = this.post.content;
+    this.imagesUrl = this.post.imagesUrl;
   }
 
   // Upload each image just after adding
@@ -74,16 +99,32 @@ export class PostDashboardComponent implements OnInit {
   }
 
   onAddPhoto(event) {
-    console.log(event);
     this.files.push(...event.addedFiles);
     for (let i = 0; i < this.files.length; i++) {
-      this.uploadImage(this.files[i], i);
+      this.uploadImage(this.files[i], i + this.imagesUrl.length);
     }
+    if (this.editMode) {this.files = []; }
   }
 
   onRemove(event) {
-      this.files.splice(this.files.indexOf(event), 1);
-      this.imagesUrl.splice(this.imagesUrl.indexOf(event), 1);
+    this.files.splice(this.files.indexOf(event), 1);
+    this.imagesUrl.splice(this.imagesUrl.indexOf(event), 1);
+  }
+
+  updatePost() {
+    const formData = {
+      title: this.title,
+      content: this.content,
+      imagesUrl: this.imagesUrl,
+    };
+    const id = this.route.snapshot.paramMap.get('id');
+    this.postService.update(id, formData);
+
+    this.snackBar.open('Post updated', 'Ok', {
+      duration: 3000,
+      panelClass: ['success-snackbar']
+    });
+    setTimeout(() => this.editFinished.emit(true),1000);
   }
 
 }
